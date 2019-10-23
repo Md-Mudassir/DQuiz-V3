bbc309110ad887e9d5a2c738e9bef924706fa6c8
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-24 00:10:24 +0530

    added quiz db

--- routes/users.js
+++ routes/users.js
@@ -88,7 +88,7 @@ router.post("/register", (req, res) => {
 // Login
 router.post("/login", (req, res, next) => {
   passport.authenticate("local", {
-    successRedirect: "/index.html",
+    successRedirect: "/home.html",
     failureRedirect: "/users/login",
     failureFlash: true
   })(req, res, next);
